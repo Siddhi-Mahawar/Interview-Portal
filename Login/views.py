27021ded0ecd1b8ec50d0d8674e707e141aaf61c
Nocturnal3003 <45755293+Nocturnal3003@users.noreply.github.com>
2021-01-09 18:25:57 +0530

    email and token

--- Login/views.py
+++ Login/views.py
@@ -6,6 +6,10 @@ from django.contrib.auth import authenticate, login
 from django.views.generic import View
 from django.http import Http404
 from Login.forms import CompanyAdminForm
+from django.core.mail import send_mail
+from django.conf import settings
+from django.utils.crypto import get_random_string
+
 
 class IndexView(generic.ListView):
     template_name = 'Login/index.html'
@@ -18,17 +22,27 @@ class DetailView(generic.DetailView):
     model = CompanyAdmin
     template_name = 'Login/detail.html'
 
-def AdminCreate(request):
 
-    context ={} 
-  
-    # create object of form 
-    form = CompanyAdminForm(request.POST or None, request.FILES or None) 
-      
-    # check if form data is valid 
-    if form.is_valid(): 
-        form.save() 
+def AdminCreate(request):
+    context = {}
+
+    form = CompanyAdminForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        save_it = form.save(commit=False)
+        save_it.save()
+        # send_mail(subject, message, from_list, to_list, fail_silently=True)
+        token = get_random_string(length=32)
+        subject = "Activation mail"
+        message = token
+        from_list = settings.EMAIL_HOST_USER
+        to_list = [save_it.email, settings.EMAIL_HOST_USER]
+        send_mail(subject, message, from_list, to_list, fail_silently=True)
         return redirect('Login:index')
 
-    context['form']= form 
-    return render(request, "Login/companyadmin_form.html", context) 
+    context['form'] = form
+    return render(request, 'Login/companyadmin_form.html', context)
+
+
+
+
