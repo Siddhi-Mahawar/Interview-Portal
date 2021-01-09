8073e7161cd04b0aa87ad57c21ab15e81c985346
Siddhi Mahawar <siddhimahawar@gmail.com>
2021-01-09 19:08:25 +0530

    Login Updated

--- Login/views.py
+++ Login/views.py
@@ -5,40 +5,54 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.views.generic import View
 from django.http import Http404
-from Login.forms import CompanyAdminForm
-from django.core.mail import send_mail
-from django.conf import settings
-from django.utils.crypto import get_random_string
+from Login.forms import CompanyAdminForm, LoginForm
+from django.contrib.auth.hashers import make_password, check_password
 
+def LoginView(request):
 
-class IndexView(generic.ListView):
-    template_name = 'Login/index.html'
+    if 'email' in request.session:
+        return redirect('Login:home')
 
-    def get_queryset(self):
-        return CompanyAdmin.objects.all()
+    context = {} 
+    
+    # create object of form 
+    form = LoginForm(request.POST or None, request.FILES or None) 
+      
+    # check if form data is valid 
+    if form.is_valid(): 
+        
+        user_email = form.cleaned_data['email']
+        user_password = form.cleaned_data['password']
+        user = CompanyAdmin.objects.get(email = user_email)
+        
+        if check_password(user_password, user.password):
+            request.session['email'] = user.email
+            return redirect('Login:home')
 
+    context['form']= form 
+    return render(request, "Login/index.html", context) 
 
-class DetailView(generic.DetailView):
-    model = CompanyAdmin
-    template_name = 'Login/detail.html'
+def HomePage(request):
 
-
-def AdminCreate(request):
-    context = {}
-
-    form = CompanyAdminForm(request.POST or None, request.FILES or None)
-
-    if form.is_valid():
-        save_it = form.save(commit=False)
-        save_it.save()
-        # send_mail(subject, message, from_list, to_list, fail_silently=True)
-        token = get_random_string(length=32)
-        subject = "Activation mail"
-        message = token
-        from_list = settings.EMAIL_HOST_USER
-        to_list = [save_it.email, settings.EMAIL_HOST_USER]
-        send_mail(subject, message, from_list, to_list, fail_silently=True)
+    if 'email' in request.session:
+        return render(request, "Login/detail.html") 
+    else:
         return redirect('Login:index')
 
-    context['form'] = form
-    return render(request, 'Login/companyadmin_form.html', context)
+def AdminCreate(request):
+
+    if 'email' in request.session:
+        return redirect('Login:home')
+
+    context = {} 
+  
+    # create object of form 
+    form = CompanyAdminForm(request.POST or None, request.FILES or None) 
+      
+    # check if form data is valid 
+    if form.is_valid(): 
+        form.save() 
+        return redirect('Login:index')
+
+    context['form']= form 
+    return render(request, "Login/companyadmin_form.html", context) 
