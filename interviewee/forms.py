373ea558a598b5220b339b86213143a25ac0c9b2
Siddhi Mahawar <siddhimahawar@gmail.com>
2021-01-12 01:27:56 +0530

    UI updated of Add Interviewee

--- interviewee/forms.py
+++ interviewee/forms.py
@@ -9,9 +9,19 @@ class IntervieweeForm(forms.ModelForm):
     class Meta:
         model = Interviewee
         fields = ['email', 'timestamp']
+        widgets = {
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+            'timestamp': forms.DateTimeInput(attrs={'placeholder': 'TimeStamp'}),
+        }
 
 class IntervieweeDetailsForm(forms.ModelForm):
 
     class Meta:
         model = Interviewee
         fields = ['name', 'phone', 'resume_link']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Enter Full Name'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Enter Phone Number'}),
+            'resume_link': forms.TextInput(attrs={'placeholder': 'Enter Resume Link'}),
+        }
