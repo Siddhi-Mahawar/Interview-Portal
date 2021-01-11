73ba34b3ca02d4095f397a0b8d9d1680b6c04e08
Siddhi Mahawar <siddhimahawar@gmail.com>
2021-01-12 01:38:36 +0530

    UI updated of Add Interviewer

--- interviewer/forms.py
+++ interviewer/forms.py
@@ -6,3 +6,11 @@ class InterviewerForm(forms.ModelForm):
     class Meta:
         model = Interviewer
         fields = ['name', 'email', 'phone', 'company_name', 'position']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Enter Full Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Enter Email Id'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Enter Phone Number'}),
+            'company_name': forms.EmailInput(attrs={'placeholder': 'Enter Company'}),
+            'position': forms.TextInput(attrs={'placeholder': 'Enter Position'}),
+        }
