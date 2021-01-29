3450413600bd89ca4ce677da9554c9ad673ca66a
Siddhi Mahawar <siddhimahawar@gmail.com>
2021-01-30 00:25:22 +0530

    Added model for Interview Room

--- editor/models.py
+++ editor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from interviewee.models import Interviewee
+from interviewer.models import Interviewer
 
 # Create your models here.
+class InterviewRoom(models.Model):
+
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    token = models.CharField(max_length=32, default="hey")
+    interviewer = models.ForeignKey(Interviewer, on_delete=models.CASCADE)
+    interviewee = models.ForeignKey(Interviewee, on_delete=models.CASCADE)
+    question = models.TextField()
