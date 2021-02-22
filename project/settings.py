6c2070f1f9a509b0bfe85a5f9cef7d63cc508cd0
Siddhi Mahawar <34399383+Siddhi-Mahawar@users.noreply.github.com>
2021-02-22 17:21:35 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -130,6 +130,6 @@ STATIC_URL = '/static/'
 # for gmail or google apps
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'dummymailwork1@gmail.com'
-EMAIL_HOST_PASSWORD = 'dummymailpass'
+EMAIL_HOST_USER = '<YOUR_EMAIL>'
+EMAIL_HOST_PASSWORD = '<YOUR_PASSWORD>'
 EMAIL_PORT = 587
